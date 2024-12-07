e04aa324b9eebb5c8dcc985a22486ac89259c4b4
Adam <adamjpolak@gmail.com>
2024-12-07 08:33:50 -0600

    fixed history page

--- Models/ImageResult.cs
+++ Models/ImageResult.cs
@@ -6,4 +6,5 @@ public class ImageResult
     public int LiquidAmount { get; set; }
     public required string DateUploaded { get; set; }
     public required string CapturedBy { get; set; }
+    public int ResultId { get; set; }
 }
